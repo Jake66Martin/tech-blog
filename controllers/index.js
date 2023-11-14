289edf31604ac89bd8be8209eb51cd13bde0890a
leviathan <jake66martin@hotmail.com>
2023-11-13 23:34:16 -0500

    added styling, login page, comment page etc

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-// const router = require('express').Router();
+
 const home = require('./homeroute');
 const registration = require('./registration')
 const login = require('./login')
+const dash = require('./dashboard')
+const comment = require('./comment')
 
 const app = express()
 
 app.use('/', home);
 app.use('/registration', registration)
 app.use('/login', login)
+app.use('/dashboard', dash)
+app.use('/comment', comment)
 
 module.exports = app;
