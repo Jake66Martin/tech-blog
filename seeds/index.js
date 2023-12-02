803af8e84fff5bdc0e40aa82c54eb96b5c52d28f
leviathan <jake66martin@hotmail.com>
2023-12-02 11:32:39 -0500

    finished

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,9 @@
 const sequelize = require('../config/connection');
-const comment = require('./comments-data');
 const user = require('./users-data');
 const blog = require('./blog-data')
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-
-  await comment();
 
   await user();
 
